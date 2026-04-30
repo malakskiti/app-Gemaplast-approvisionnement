4c35785325eebf409306bff118bbc37e00e13da8
malakskiti <malakskiti5@gmail.com>
2026-04-30 23:52:59 +0100

    Change background color of KPI card to red

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.markdown("""
 
    /* Cases KPI et En-tête Nouvelle saisie */
 .kpi-card, .st-emotion-cache-p6495m {
-    background-color: #6e0b14 !important; /* On remplace #262730 par #454754 */
+    background-color: #CC0000 !important; /* On remplace #262730 par #454754 */
     color: white !important;
 }
     /* Style spécifique pour l'en-tête de l'expander (formulaire) */
